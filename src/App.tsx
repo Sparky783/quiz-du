377d4228ae04ddd3c93587c4e9a910e26c24b5cd
Florent Lavignotte <f.lavignotte-saint-laurent@cgi.com>
2026-01-15 12:44:20 +0100

    Add footer with the number of questions

--- src/App.tsx
+++ src/App.tsx
@@ -24,33 +24,41 @@ function App() {
       const questionNumber = Math.floor(Math.random() * totalQuestions)
       return (questions as any)[questionNumber]
     })
-    console.log(question)
   }
 
   useEffect(() => {
-    console.log('Loading question...', questions)
     loadQuestion()
   }, [])
 
   return (
     <>
-      <h1>Quiz DU</h1>
-      <p>
-        Question {questionNumber}
-      </p>
-      <div className="question-card">
-        <p className='question-text'>
-          {question.text}
+      <div className="content">
+        <h1>Quiz DU</h1>
+        <p>
+          Question {questionNumber}
         </p>
+        <div className="question-card">
+          <p className='question-text'>
+            {question.text}
+          </p>
 
-        {showAnswer &&
-          <p className='question-answer'>{question.answer}</p>
-        }
+          {showAnswer &&
+            <p className='question-answer'>{question.answer}</p>
+          }
+        </div>
+
+        <button onClick={revealAnswer}>
+          {showAnswer ? "Révéler la réponse" : "Question suivante"}
+        </button>
       </div>
-
-      <button onClick={revealAnswer}>
-        {showAnswer ? "Révéler la réponse" : "Question suivante"}
-      </button>
+      <div className="footer">
+        <p>
+          Base de données de {Object.keys(questions).length} questions sur le DU.
+        </p>
+        <p>
+          Made with ❤️ by <a href="https://github.com/florentlavignotte">Florent Lavignotte</a>
+        </p>
+      </div>
     </>
   )
 }
